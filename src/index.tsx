025dd25f0a25dfe940b59c628058b89dc52f1825
amirali26 <amiralishojaee123@gmail.com>
2022-05-28 12:52:39 +0100

    updated package versions

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Router history={history as any}>
+        <Router history={history}>
           <App />
         </Router>
       </ThemeProvider>
